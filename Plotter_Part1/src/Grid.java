31603c61ef31e7f5d8e3437ee869a2b9ad12ec37
Sebastian Baur <herr_baur@gmx.de>
2022-04-04 17:57:27 +0200

    stage

--- Plotter_Part1/src/Grid.java
+++ Plotter_Part1/src/Grid.java
@@ -26,7 +26,14 @@ public class Grid extends JFrame {
      * coordinates zeichnet ein Koordinatenkreuz.
      */
     public void coordinates(Graphics g) {
-
+        int[] xAxis = new int[4];
+        int[] yAxis = new int[4];
+        xAxis[1] = this.getWidth()/4;
+        yAxis[1] = this.getHeight()/4;
+        xAxis[2] = this.getWidth()*3/4;
+        yAxis[2] = this.getHeight()*3/4;
+        g.drawLine(xAxis[1], yAxis[2], xAxis[2], yAxis[2]);
+		g.drawLine(xAxis[1], yAxis[1], xAxis[1], yAxis[2]);
     }
 
     // TODO: implementiere eine Achsenbeschriftung
@@ -34,7 +41,7 @@ public class Grid extends JFrame {
      * labels zeichnet Achsenbeschriftungen bei ganzen Zahlen.
      */
     public void labels(Graphics g) {
-
+        
     }
 
     // TODO: implementiere einen Funktionsplot.
