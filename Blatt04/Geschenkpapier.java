059108a0daca4b660eae3dbc9d15517a3881f1a2
Sebastian Baur <herr_baur@gmx.de>
2022-04-07 12:27:17 +0200

    Blatt 4 fertig

--- Blatt04/Geschenkpapier.java
+++ Blatt04/Geschenkpapier.java
@@ -5,55 +5,57 @@ import javax.swing.JFrame;
 
 public class Geschenkpapier extends JFrame {
 
-    public static int width = 1024;
-    public static int height = 1024;
+	public static int width = 1024;
+	public static int height = 1024;
 
-    public Geschenkpapier() {
-        super();
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(1024, 1024);
-        this.setTitle("Schnitzel mit Pommes" + this.getClass().getName());
-        this.setVisible(true);
-    }
+	public Geschenkpapier() {
+		super();
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(width, height);
+		this.setTitle("Geschenkpapier" + this.getClass().getName());
+		this.setVisible(true);
+	}
 
-    public static void main(String[] args) {
-        new Geschenkpapier();
-    }
+	public int getRandomNumber(int min, int max) {
+		return (int) ((Math.random() * (max - min)) + min);
+	}
 
-    public void paint(Graphics g) {
-        for (int anzahl = 10; anzahl > 0; anzahl--) {
-            this.Stern(g); // Aufruf der selbst programmierten Methode
-        }
-    }
+	public static void main(String[] args) {
+		new Geschenkpapier();
+	}
 
-    public void Stern(Graphics g) {
+	public void paint(Graphics g) {
+		
+		g.clearRect(0, 0, this.getWidth(), this.getHeight());
+		this.meineZufallsSterne(g); 
+	}
 
-        double Gradschritte = 15;
-        double radius = 50 + 100 * Math.random();
-        int[] xKoordinate = new int[100];
-        int[] yKoordinate = new int[100];
-        double xMitte = this.getWidth() / 20 + (this.getWidth()*18/20) * Math.random();
-        double yMitte = this.getHeight() / 20 + (this.getHeight()*18/20) * Math.random();
-        
-        xKoordinate[0] = (int)xMitte;
-        yKoordinate[0] = (int)yMitte;
-               
-        for (int zaehler = 1; zaehler <= (360 / Gradschritte); zaehler++) {
-            xKoordinate[1] = (int) (xKoordinate[0] + radius * Math.sin(zaehler * Math.toRadians(Gradschritte)));
-            yKoordinate[1] = (int) (yKoordinate[0] + radius * Math.cos(zaehler * Math.toRadians(Gradschritte)));
-            g.setColor(Color.BLUE);
-            g.drawLine(xKoordinate[0], yKoordinate[0], xKoordinate[1], yKoordinate[1]);
-        }
-        ;
-        for (int zaehler = 1; zaehler <= (360 / Gradschritte); zaehler++) {
-            xKoordinate[2] = (int) (xKoordinate[0] + (radius / 2)
-                    * Math.sin(zaehler * Math.toRadians(Gradschritte) + Math.toRadians(Gradschritte / 2)));
-            yKoordinate[2] = (int) (yKoordinate[0] + (radius / 2)
-                    * Math.cos(zaehler * Math.toRadians(Gradschritte) + Math.toRadians(Gradschritte / 2)));
-            g.setColor(Color.MAGENTA);
-            g.drawLine(xKoordinate[0], yKoordinate[0], xKoordinate[2], yKoordinate[2]);
-        }
-        ;
+	public void meineZufallsSterne(Graphics g) {
+		for (int anzahl = 30; anzahl>0; anzahl--) {
+			int r = getRandomNumber(150, 50);
+			int xMitte = (int) (this.getWidth()/20+(Math.random() * this.getWidth())*18/20);
+			int yMitte = (int) (this.getWidth()/20+(Math.random() * this.getHeight())*18/20);
 
-    }
+			this.zeichneEinenStern(g, xMitte, yMitte, r);
+		}
+	}
+
+	public void zeichneEinenStern(Graphics g, int xMitte, int yMitte, int r) {
+
+		double gradSchritte = 15;
+
+		for (int zaehler = 1; zaehler <= (360 / gradSchritte); zaehler++) {
+			int xCoord_1 = (int) (xMitte + r * Math.sin(zaehler * Math.toRadians(gradSchritte)));
+			int yCoord_1 = (int) (yMitte + r * Math.cos(zaehler * Math.toRadians(gradSchritte)));
+			g.setColor(Color.BLUE);
+			g.drawLine(xMitte, yMitte, xCoord_1, yCoord_1);
+		}
+
+		for (int zaehler = 1; zaehler <= (360 / gradSchritte); zaehler++) {
+			int xCoord_2 = (int) (xMitte + (r / 2) * Math.sin(zaehler * Math.toRadians(gradSchritte) + Math.toRadians(gradSchritte / 2)));
+			int yCoord_2 = (int) (yMitte + (r / 2) * Math.cos(zaehler * Math.toRadians(gradSchritte) + Math.toRadians(gradSchritte / 2)));
+			g.setColor(Color.MAGENTA);
+			g.drawLine(xMitte, yMitte, xCoord_2, yCoord_2);
+		}
+	}
 }
