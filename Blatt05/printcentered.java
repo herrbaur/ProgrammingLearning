4bc195febd929cf9710384a035b318087ec26e3d
herrbaur <herr_baur@gmx.de>
2022-04-09 21:28:19 +0200

    fixed printcentered array overflow

--- Blatt05/printcentered.java
+++ Blatt05/printcentered.java
@@ -1,32 +1,24 @@
 package Blatt05;
-
 public class printcentered {
     public printcentered(String Liedtext) {
         int maxCount = 0;
-        String[] Liedformatiert;
         String[] Liedteil = Liedtext.split("\n");
-        int[] anzahl = new int[3];
+        int[] anzahl = new int[4];
 
         for (int i = 0; i < 4; i++) {
             anzahl[i] = Liedteil[i].length();
             if (anzahl[i] > maxCount) {
                 maxCount = anzahl[i];
-                }  
-            if (i==3){
-                for (int j = 0; j < 4; j++) {
-                    if (Liedteil[j].length() < maxCount){
-                        String Einrückung ="/t";
-                        int blanks = (maxCount-Liedteil[j].length())/2;
-                            for (;blanks > 0; blanks--) {
-                                Liedteil[j] = Einrückung + Liedteil[j];
-                            }  
-                    }
-                    System.out.println(Liedteil[j]);
-                    System.out.println("hallo");
-                }  
             }
         }
-
-        
+        for (int j = 0; j < 4; j++) {
+            if (Liedteil[j].length() < maxCount) {
+                int blanks = (maxCount - Liedteil[j].length()) / 2;
+                for (; blanks > 0; blanks--) {
+                    Liedteil[j] = " " + Liedteil[j];
+                }
+            }
+            System.out.println(Liedteil[j]);
+        }
     }
 }
