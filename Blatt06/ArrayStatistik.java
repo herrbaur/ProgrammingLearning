c7c769d5faa67d47c5270796e587d79a3f3e0a1c
Sebastian Baur <herr_baur@gmx.de>
2022-05-16 10:44:17 +0200

    changes from the last 3 weeks

---
+++ Blatt06/ArrayStatistik.java
@@ -0,0 +1,8 @@
+package Blatt06;
+
+public class ArrayStatistik {
+    int[] Zahlen = {35, 7, 90, 26, 92, 54, 24, 65, 2, 67, 43, 26, 80, 92, 7, 40, 67, 66, 31, 45, 7, 100, 96, 93, 12, 20, 57, 22, 62, 51};
+    public static void main (String[]args){
+        System.out.println(Zahlen);
+    }
+}
